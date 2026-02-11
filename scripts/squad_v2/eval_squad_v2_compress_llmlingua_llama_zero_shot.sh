0a194560ee6a36205e1667e940676b129567b495
EvelynHung-79 <chia20010709@gmail.com>
2026-02-11 15:38:58 +0800

    remove non-need code

--- scripts/squad_v2/eval_squad_v2_compress_llmlingua_llama_zero_shot.sh
+++
@@ -1,4 +0,0 @@
-PYTHONPATH=. python3 -m accelerate.commands.launch --config_file \
-  conf/accelerate/multi_gpu.yaml \
-  src/context_compression/run.py --multirun +experiments_squad_2=evaluate_llmlingua_compress_zeroshot_qa_squad \
-  models.target_token=256
